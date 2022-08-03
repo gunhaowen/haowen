231a310cbfcd67a4e91f92005a82d13d3de2f5f6
Haowen <asd25878098@gmail.com>
2022-08-03 15:07:20 +0800

    change main css

--- src/page/homePage/HomePage.tsx
+++ src/page/homePage/HomePage.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Header, Footer } from "../../components";
 import { MainLayout } from "../../layout/mainLayout";
 import styles from "./HomePage.module.css";
 import { Row, Col } from "antd";
 import a from "../../assets/images/a.jpg";
-import { LeftOutlined } from "@ant-design/icons";
 
 export const HomePage: React.FC = () => {
   return (
@@ -12,20 +10,22 @@ export const HomePage: React.FC = () => {
       <div className={styles.main}>
         <Row>
           <Col span={12}>
-            <h3 className={styles["main-h3"]} style={{ marginTop: "27%" }}>
-              hi~ 我是 Haowen{" "}
-            </h3>
-            <h3 className={styles["main-h3"]}>一位網頁前端設計師</h3>
-            <p className={styles["main-p"]}>熱衷於網頁開發</p>
-            <p className={styles["main-p"]}>平常喜歡用影片記錄旅遊</p>
+            <div className={styles["main-special"]}>
+              <h3 className={styles["main-h3"]}>hi~ 我是 Haowen </h3>
+              <h3 className={styles["main-h3"]}>一位網頁前端設計師</h3>
+              <p className={styles["main-p"]}>熱衷於網頁開發</p>
+              <p className={styles["main-p"]}>平常喜歡用影片記錄旅遊</p>
+            </div>
           </Col>
           <Col span={12}>
-            <img
-              src={a}
-              height={500}
-              width={450}
-              className={styles["main-img"]}
-            />
+            <div className={styles["main-right"]}>
+              <img
+                src={a}
+                height={500}
+                width={450}
+                className={styles["main-img"]}
+              />
+            </div>
           </Col>
         </Row>
       </div>
